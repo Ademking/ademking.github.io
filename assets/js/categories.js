f4b7ab7a29f0fb3d0fd07984ca2d1f01466014d0
Ademking <ademkingnew@gmail.com>
2023-01-06 04:04:41 +0100

    Remove log

--- assets/js/categories.js
+++ assets/js/categories.js
@@ -3,7 +3,6 @@
 
 const categories = { {% for category in site.categories %}{% capture category_name %}{{ category | first }}{% endcapture %}{{ category_name | replace: " ", "_" }}: [{% for post in site.categories[category_name] %}{ url: `{{ site.baseurl }}{{ post.url }}`, date: `{{post.date | date_to_string}}`, title: `{{post.title}}`},{% endfor %}],{% endfor %} }
 
-console.log(categories)
 
 window.onload = function () {
   document.querySelectorAll(".category").forEach((category) => {
