5bbcf0d41bb312220545f447376b1b38cd2d0fdb
Ademking <ademkingnew@gmail.com>
2023-01-11 03:47:25 +0100

    Add copy to clipboard btn

---
+++ assets/js/codeblock.js
@@ -0,0 +1,19 @@
+document.querySelectorAll("div.highlight").forEach(function (pre) {
+  var button = document.createElement("button");
+  var copyText = "Copy";
+  button.className = "copy";
+  button.type = "button";
+  button.ariaLabel = "Copy code to clipboard";
+  button.innerText = copyText;
+  button.addEventListener("click", function () {
+    var code = pre.querySelector("code").innerText.trim();
+    navigator.clipboard.writeText(code);
+    button.innerText = "Copyied!";
+    button.style.left = "calc(100% - 6.5rem)";
+    setTimeout(function () {
+      button.innerText = copyText;
+      button.style.left = "calc(100% - 4.5rem)";
+    }, 4000);
+  });
+  pre.prepend(button);
+});
