edc052f969cd89273380576ff06db61f6a0ec3dd
Ademking <ademkingnew@gmail.com>
2023-01-13 00:33:50 +0100

    Update cookie

--- assets/js/confetti.js
+++ assets/js/confetti.js
@@ -1,10 +1,24 @@
-let cookieElem = document.querySelector("#cookie-emoji");
-cookieElem.addEventListener("click", () => {
-  const jsConfetti = new JSConfetti();
-  jsConfetti.addConfetti({
-    emojis: ["🍪"],
-    emojiSize: 100,
-    confettiRadius: 1,
-    confettiNumber: 20,
+let cookieElems = document.querySelectorAll(".em-cookie");
+cookieElems.forEach((elem) => {
+  elem.style.cursor = "pointer";
+  elem.addEventListener("click", (ev) => {
+    ev.preventDefault();
+    const jsConfetti = new JSConfetti();
+
+    const img = new Image();
+    img.src = "/assets/icons/cookie2.png";
+    let imgWidth = window.innerWidth / 20;
+    console.log("imgWidth", imgWidth);
+
+    jsConfetti.addConfetti({
+      images: [
+        {
+          src: img,
+          width: imgWidth,
+        },
+      ],
+      confettiRadius: 1,
+      confettiNumber: 20,
+    });
   });
 });
